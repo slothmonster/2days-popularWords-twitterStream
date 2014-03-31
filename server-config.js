64c717df1a06648cada2b055a70473874519d065
Adam Deibert <ardeibert@gmail.com>
2014-03-31 12:30:41 -0700

    initial commit - basic node server

---
+++ server-config.js
@@ -0,0 +1,11 @@
+var express = require('express');
+// var util = require('./lib/utility');
+// var handler = require('./lib/request-handler');
+
+var app = express();
+
+app.configure(function() {
+  app.use(express.bodyParser());
+});
+
+module.exports = app;
